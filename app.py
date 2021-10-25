77071292db3351c13ab11905615b1b5a92f73712
amanchauhan71 <amanchauhan7172@gmail.com>
2021-10-25 13:17:37 +0530

    PORT ADDED

--- app.py
+++ app.py
@@ -49,7 +49,8 @@ def math_operation_via_postman():
 
 
 if __name__ == '__main__':
-    host = "127.0.0.1"
+    host = "33.44.55.66"
+    port = "500"
     app.run(host="127.0.0.1", port=8080,debug=True)
 # Class Defination for flask
 
