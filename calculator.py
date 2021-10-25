ee46afc72e0efc30f04548ac2e7690875070929e
amanchauhan71 <amanchauhan7172@gmail.com>
2021-10-25 12:48:03 +0530

    Line Remove in Calculator file

--- calculator.py
+++ calculator.py
@@ -27,11 +27,3 @@ class Calculator:
         print(result)
         return result
 
-
-
-if __name__ == '__main__':
-    operation = Calculator()
-    operation.add()
-    operation.subtract()
-    operation.multiply()
-    operation.divide()
